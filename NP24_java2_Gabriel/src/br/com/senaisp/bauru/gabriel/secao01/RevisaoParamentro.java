9c5a16589b57231f3c40cf610faeeb1524543dd1
gabrielmeirap <gabrielpmeira@icloud.com>
2024-03-13 20:05:56 -0300

    Commit inicial

---
+++ NP24_java2_Gabriel/src/br/com/senaisp/bauru/gabriel/secao01/RevisaoParamentro.java
@@ -0,0 +1,12 @@
+package br.com.senaisp.bauru.gabriel.secao01;
+
+public class RevisaoParamentro {
+
+	public static void main(String[] args) {
+    for(String item : args) {
+    	System.out.println(item); 
+    }
+    
+}
+
+}
