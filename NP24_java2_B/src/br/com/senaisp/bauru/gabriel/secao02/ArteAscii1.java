b353730ad38bc915732f2c72c629c8da48e1d3e8
gabrielmeirap <gabrielpmeira@icloud.com>
2024-03-13 22:08:53 -0300

    conteudo da aula dia 13-03-2024

---
+++ NP24_java2_B/src/br/com/senaisp/bauru/gabriel/secao02/ArteAscii1.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.gabriel.secao02;
+
+public class ArteAscii1 {
+
+	public static void main(String[] args) {
+       System.out.println("  ▓▓▓▓  ");
+       System.out.println(" ▓    ▓");
+       System.out.println("▓ ▓  ▓ ▓");
+       System.out.println("▓      ▓");
+       System.out.println("▓ ▓  ▓ ▓");
+       System.out.println("▓  ▓▓  ▓");
+       System.out.println(" ▓    ▓ "   );
+       System.out.println("  ▓▓▓▓");
+	}
+
+}
