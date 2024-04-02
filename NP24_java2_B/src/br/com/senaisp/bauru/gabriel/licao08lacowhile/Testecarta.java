72a806af2d52606179261c46210a4b3ab2035684
gabrielmeirap <gabrielpmeira@icloud.com>
2024-04-01 22:04:09 -0300

    commit do dia 01-04-24

---
+++ NP24_java2_B/src/br/com/senaisp/bauru/gabriel/licao08lacowhile/Testecarta.java
@@ -0,0 +1,8 @@
+package br.com.senaisp.bauru.gabriel.licao08lacowhile;
+
+public class Testecarta {
+    public static void main(String[] args) {
+     Carta ct = new Carta (3,9); //10 de paus 
+     System.out.println(ct);
+    }
+}
