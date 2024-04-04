5fb04f9f3e095d0bd7ef6a8b845e1906bf6e4106
gabrielmeirap <gabrielpmeira@icloud.com>
2024-04-03 21:58:07 -0300

    commit do dia 03-04-24

--- NP24_java2_B/src/br/com/senaisp/bauru/gabriel/licao08lacowhile/ArrayListEx2.java
+++ NP24_java2_B/src/br/com/senaisp/bauru/gabriel/licao08lacowhile/ArrayListEx2.java
@@ -4,54 +4,47 @@ import java.util.Random;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.ListIterator;
-import java.util.Random;
-
 public class ArrayListEx2 {
     public static void main(String args[]) {
-        ArrayList<Integer> lstInt = new ArrayList<Integer>();
-        Random rnd = new Random();
-        // adicionando valores aleatorios de 1 a 200 a lista
-        for (int i = 0; i < 100; i++) {
-            lstInt.add(rnd.nextInt(200) + 1);
+        ArrayList<Integer> IstInt = new ArrayList<Integer>();
+        Random rnd = new Random ();
+        //Adicionado valores aleatórios de 1 a 200 na lista
+        for (int i=0;1<100;i++) {
+        	IstInt.add(rnd.nextInt(200)+1);
+        	
         }
-        // listar os valores gerados
-        for (int it : lstInt) {
-            System.out.println(it);
+        //listar os valores gerados 
+        for (int it: IstInt) {
+        	System.out.println(it);
         }
-        // Utilizando o iterator
+        //Utilizando o iterador
         System.out.println("Listando usando iterator");
-        Iterator<Integer> iter = lstInt.iterator();
+        Iterator<Integer> iter = IstInt.iterator();
         while (iter.hasNext()) {
-            System.out.println(iter.next());
+        	System.out.println(iter.next());
         }
-        
-        // removendo os pares> isso pode dar problema
-        //porque eu perco a referencia dos indices
+        //removendo os pares => Isso pode dar problemas 
+        //porque eu perco a referencia de indices 
         /*
-        for (int it : lstInt) {
-            if (it % 2 == 0) {
-                // removendo o item
-                lstInt.remove(it);
-//                
-            }
+        for (int it : IstInt) {
+        	if (it%2==0) {
+        		//removendo o item par
+        		IstInt.remove(it);
+        	}
         }
-        */
-        //para resolver problemas
-        for (int i = lstInt.size()-1;i>0;i--) {
-            if (lstInt.get(i)%2==0) {
-                
-            }
+        //Para resolver o problema, removendo a partir do 
+         //indice do maior para o menor
+          *   
+          */
+        
+        for (int i=IstInt.size()-1;i>0;i--) {
+        	if (Istint.get(i)%2==0;i--)
         }
-        // Listando o resultante depois de retirado os valores
-        // Utilizando o ListIterator
-        ListIterator<Integer> lstIt = lstInt.listIterator();
-        while (lstIt.hasNext()) {
-            System.out.println(lstIt.next());
-        }
-        System.out.println("Mostrando o ultimo novamente");
-        System.out.println(lstIt.previous()); //volta um indice
+        //Listando o resultante depois ed retirado os valores 
+        //Utilizando o listIterator
+        ListIterator<Integer> IsIt = IstInt.listIterator();
+        while (IsIt.hasNext());
+           System.out.println(IsIt.nextIndex());
+         
     }
 }
--- Projeto_BancoDados/src/br/com/semaisp/bauru/gabriel/testers/TesteProdutoSimples.java
+++ Projeto_BancoDados/src/br/com/semaisp/bauru/gabriel/testers/TesteProdutoSimples.java
@@ -1,5 +1,74 @@
 package br.com.semaisp.bauru.gabriel.testers;
 
-public class TesteProdutoSimples {
+import java.sql.SQLException;
+import java.util.Scanner;
 
-}
+import br.com.senaisp.bauru.gabriel.classes.ProdutoSimples;
+
+public class TesteProdutoSimples {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner (System.in);
+                 int op;
+                 do {
+                     System.out.println("Menu");
+                    System.out.println("1 - Criar produto");
+                    System.out.println("2 - Consultar produto");
+                    System.out.println("3 - Alterar produto");
+                   System.out.println("4- Excluir produto");
+                   System.out.println("9 Fim");
+                   op = sc.nextInt();
+                   switch (op) {
+                   case 1 -> criarProduto (sc);
+                   case 2 -> consultarProduto (sc);
+                   case 3 -> alterarProduto (sc);
+                   case 4 -> excluirProduto (sc);
+                   case 9 -> System.out.println("Bye");
+                  default -> System.out.println("Opção invalida");
+                   }
+        }while (op!=9);
+    }
+    private static void excluirProduto(Scanner sc) {
+        // TODO Auto-generated method stub
+        
+    }
+    private static void alterarProduto(Scanner sc) {
+        // TODO Auto-generated method stub
+        
+    }
+    private static void consultarProduto(Scanner sc) {
+        // TODO Auto-generated method stub
+        
+    }
+    private static void criarProduto(Scanner sc) {
+        System.out.println("Cadastramento de produto");
+        System.out.println("Digite a descrição do produto:");
+        int cod = sc.nextInt();
+        try {
+			//Pesquisando o produto
+			ProdutoSimples prd = ProdutoSimples.findByPK(cod);
+			//Mostrando o produto
+			System.out.println(prd);
+		} catch (Exception e1) {
+          System.out.println(e.getMessage());
+		}
+        //Pausa para voltar para o menu
+        System.out.println("Digite o saldo do produtp");
+        int sal = sc.nextInt();
+        
+        System.out.println("Digite o custo do produto");
+        double cus = sc.nextDouble();
+        
+        try {
+            //Criando o produto
+            ProdutoSimples prd = ProdutoSimples.create(des, sal, cus);
+            //Mostrando o produto
+            System.out.println(prd);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
+        }
+        //Pausa para voltar para menu
+        System.out.println("Digite enter para voltar para menu");
+        sc.nextLine();
+    }
+    }
